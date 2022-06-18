dcee20eb951580ded9359ef86e19752f76ce1dd2
KrisAirdancer <chris.scott.marston@gmail.com>
2022-06-18 14:31:34 -0600

    Completed the first part of the NodeJS basics.

---
+++ global.js
@@ -0,0 +1,25 @@
+// Global Objects
+    // Global objects are available to all parts of the application's code.
+    // This file represents a global object.
+    // All files have access to the built-in global variables and methods, several methods are automatically created for us to use.
+        // You can see these by running node global in the terminal
+        // You can also make your own global variables and functions.
+    
+
+// console.log(global);
+
+// This is one of the provided global methods.
+// This creates a 3 second wait, then prints "in the timeout".
+// global.setTimeout( () => {
+//     console.log('in the timeout');
+//     clearInterval(int); // This line causes the setInterval method to be called during the timeout so that it stops running when the timeout does.
+// }, 3000);
+
+// // // This runs indefinitely unless you kill it in the console.
+// const int = setInterval( () => {
+//     console.log('in the interval');
+// }, 1000);
+
+// console.log(__dirname); // Gets the absoulte path of the directory that this file is in. It does not include the filename.
+// console.log(__filename); // Gets the absoulte path of this file (filename included).
+
---
+++ test.js
@@ -0,0 +1,10 @@
+// const player = 'yoshi';
+
+// console.log(player);
+
+const greet = firstName => {
+    console.log(`Hello, ${firstName}`);
+}
+
+greet('Mario');
+greet('Yoshi');
