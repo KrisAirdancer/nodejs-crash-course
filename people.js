6c9eedf93049e1f5d112e75addec90b10c8f8c48
KrisAirdancer <chris.scott.marston@gmail.com>
2022-06-18 14:55:55 -0600

    Learning about modules, imports, and exports.

---
+++ people.js
@@ -0,0 +1,9 @@
+const people = ['Haruhi', 'Kyon', 'Yuki', 'Mikuru', 'Koizumi'];
+const ages = [15, 16, 15, 17, 16];
+
+// console.log(people);
+
+module.exports = {
+    people,
+    ages
+}; // This data is transferred out of this file and can be imported into other files.
