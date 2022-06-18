ba7f9458c4c2f254362d8ec840a39ffb639d765a
KrisAirdancer <chris.scott.marston@gmail.com>
2022-06-18 17:32:27 -0600

    Learned how to recieve messages from the browser and how to send messages to the browser.

--- server.js
+++ server.js
@@ -12,6 +12,23 @@ const http = require('http');
 // This runs every time a request is made of the server by the browser. The callback fires every time this method is called, therefore, it fires every time the browser sends a message to the server.
 const server = http.createServer( (req, res) => { // req = request (incomming messages from browser), res = response (outgoing messages written and sent by us)
     console.log('request made');
+    // console.log(req); // This returns the entire request object contents.
+    console.log(req.url, req.method); // If you type just 'req', the entire request will be printed. No need to call toString
+
+    // The following are the slopy way to send text. Use a .html file instead.
+
+    // Headers are used by the browser to determine what kind of information it is recieving. Such as data type (JSON, HTML, etc.) or setting cookies.
+    // Set header content type
+    // res.setHeader('Content-Type', 'text/plain'); // Sending plain text to the browser.
+    // res.write("Hello world!"); // This sends information back to the browser.
+    
+    res.setHeader('Content-Type', 'text/html');
+    // The browser will automatically add header html tags to the site if we don't. But we did, so it won't.
+    res.write('<head><link rel="styleseet" href="#"></head>')
+    res.write('<p>Hello world!</p>');
+    res.write('<p>Hello slime mold!</p>');
+
+    res.end(); // Ends the response and sends the information to the browser. This is the step that sends the data.
 });
 
 // This tells the server to listen on the specified port at the specified address.
